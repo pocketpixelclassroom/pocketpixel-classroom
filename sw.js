67a9139f820d75fff14e99276ec5c5559ec83bb7
pocketpixelclassroom <pocketpixelclassroom@gmail.com>
2026-02-05 18:50:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "qr-modules-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./module1.html",
+  "./module2.html",
+  "./style.css",
+  "./script.js"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('qr-offline-cache').then(cache => {
-      return cache.addAll([
-        './communication.html',
-        './money.html',
-        './style.css',
-        './script.js'
-      ]);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
